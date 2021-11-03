3784455bafe66c40f2bf2306aa96a26445b9a6ea
peterrevsbech <peter.revsbech@gmail.com>
2021-11-03 16:16:44 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Osmand3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Osmand3.dir/main.c.obj"
+  "Osmand3.exe"
+  "Osmand3.exe.manifest"
+  "Osmand3.pdb"
+  "libOsmand3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Osmand3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
